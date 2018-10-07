89a5effad2f8fa055c3316f5d243d0a2fae3825f
dylan <meeusdylan@protonmail.com>
2018-10-08 00:44:52 +0200

    add LoL engine

---
+++ LispPlayground/LandOfLisp/engine.lisp
@@ -0,0 +1,46 @@
+
+; text-based game engine
+
+(defparameter *nodes* '((living-room (you are in the living-room.
+                                        A wizard is snoring loudly on the couch.))
+                        (garden (you are in a beautiful garden. 
+                                     There is a well in front of you.))
+                        (attic (you are in the attic.
+                                    There is a giant welding torch in the corner))
+                        ))
+
+
+(defparameter *edges* '((living-room (garden west door)
+                                     (attic upstairs ladder)
+                                     )
+                        (garden (living-room east door))
+                        (attic (living-room downstairs ladder))
+                        ))
+
+(defparameter *objects* '(whiskey bucket frog chain))
+
+(defparameter *object-locations* '((whiskey living-room)
+                                   (bucket living-room)
+                                   (chain garden)
+                                   (frog garden)))
+
+
+(defun describe-location (location nodes)
+  (cadr (assoc location nodes)))
+
+(defun describe-path (edge)
+  `(there is a, (caddr edge) going, (cadr edge) from here.))
+
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
+
+
+(defun objects-at (loc objs obj-locs)
+  (labels ((at-loc-p (obj)
+                     (eq (cadr (assoc obj obj-locs)) loc)))
+                 (remove-if-not #'at-loc-p objs)))
+
+(defun describe-objects (loc objs obj-loc)
+  (labels ((describe-obj (obj)
+                         `(you see a, obj on the floor.)))
+    (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
