77ef43be74f2782587384d460b842ad8d5445390
dylan <meeusdylan@protonmail.com>
2018-10-11 00:36:05 +0200

    add repl

--- LispPlayground/LandOfLisp/engine.lisp
+++ LispPlayground/LandOfLisp/engine.lisp
@@ -1,5 +1,7 @@
-
 ; text-based game engine
+
+; DATA
+
 (defparameter *location* 'living-room)
 
 (defparameter *nodes* '((living-room (you are in the living-room.
@@ -24,6 +26,11 @@
                                    (bucket living-room)
                                    (chain garden)
                                    (frog garden)))
+
+
+(defparameter *allowed-commands* '(look walk pickup inventory))
+
+; FUNCTIONS
 
 
 (defun describe-location (location nodes)
@@ -70,3 +77,47 @@
 
 (defun inventory() 
   (cons 'items-(objects-at 'body *objects* *object-locations*)))
+
+
+;;;;;;;;;;;;;;;;; GAME REPL
+
+(defun game-repl()
+  (let ((cmd (game-read)))
+    (unless (eq (car cmd) 'quit)
+      (game-print (game-eval cmd))
+      (game-repl)))
+  )
+            
+(defun game-read()
+  (let ((cmd (read-from-string
+               (concatenate 'string "(" (read-line) ")"))))
+    (flet ((quote-it (x)
+                     (list 'quote x)))
+      (cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
+
+(defun game-eval(sexp) 
+  (if (member (car sexp) *allowed-commands*)
+    (eval sexp)
+    '(I do not know that command.)))
+
+(defun tweak-text(lst caps lit)
+  (when lst
+    (let ((item (car lst))
+          (rest (cdr lst)))
+      (cond ((eq item #\space) (cons item (tweak-text rest caps lit)))
+            ((member item '(#\! #\? #\.)) (cons item (tweak-text rest t lit)))
+            ((eq item #\") (tweak-text rest caps (not lit)))
+            (lit (cons item (tweak-text rest nil lit)))
+
+      ((or caps lit) (cons (char-upcase item) (tweak-text rest nil lit)))
+      (t (cons (char-downcase item) (tweak-text rest nil nil)))))))
+
+
+(defun game-print (lst)
+  (princ (coerce (tweak-text (coerce (string-trim "()"
+                                                  (prin1-to-string lst))
+                                     'list)
+                             t
+                             nil)
+                 'string))
+         (fresh-line))
