cd8174dda2ec2085a6818becb1e1e8758962d411
dylan <meeusdylan@protonmail.com>
2019-04-13 03:29:05 +0200

    Add write/read methods

--- PracticalCL/db.lisp
+++ PracticalCL/db.lisp
@@ -28,3 +28,15 @@
 (defun add-cds()
   (loop (add-record (prompt-for-cd))
         (if (not (y-or-n-p "Another?")) (return))))
+
+(defun save-db (filename)
+  (with-open-file (out filename
+                       :direction :output
+                       :if-exists :supersede)
+    (with-standard-io-syntax
+      (print *db* out))))
+
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax
+      (setf *db* (read in)))))
